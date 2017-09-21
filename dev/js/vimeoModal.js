de2e5449d9a0668f3ffce121201b8d17206f3685
Alvaro Felipe <alvaro@ed.team>
2017-09-20 19:27:36 -0500

    Creados componentes javascript de desarrollo Se agrega modal de Vimeo

---
+++ dev/js/vimeoModal.js
@@ -0,0 +1,20 @@
+const createVimeoModalContent = vimeoCode =>
+`<div class="video">
+  <iframe src="https://player.vimeo.com/video/${vimeoCode}?autoplay=1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>
+</div>`;
+
+const getVimeoCode = url => url.slice('https://vimeo.com/'.length);
+
+// Evento para abrir los modales en todos los links
+const openVimeoModal = selector => {
+  let linksElements = [...document.querySelectorAll(selector)],
+    links = linksElements.map(link => link.href);
+    console.log(linksElements);
+
+  linksElements.forEach((el,i) => {
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      printModal(createVimeoModalContent(getVimeoCode(links[i])));
+    })
+  })
+};
